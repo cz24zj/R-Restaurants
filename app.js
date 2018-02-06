c810eebf6b96ac3eb0e8b8dcdac35f2617e16ae1
JUN <zj345196443@gmail.com>
2018-02-06 06:00:16 +0000

    app.js change

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var RateLimit = require('express-rate-limit');
 app.enable('trust proxy');
 app.use(express.static(__dirname +'/public'));
 // mongoose.connect('mongodb://localhost/restaurants');
-mongoose.connect('mongodb://cz24zj:czzj93723@ds046867.mlab.com:46867/r-restaurants');
+mongoose.connect(mlaburl);
 app.use(bodyparser.urlencoded({extended:true}));
 app.set('view engine','ejs');
 app.use(methodoverride('_method'));
